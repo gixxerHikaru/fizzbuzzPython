f7e0480024643e13529e63b6554c93cf9c16caaa
gixxerHikaru <yuki.sakurai0617@gmail.com>
2024-07-08 23:21:44 +0900

    リファクタリング

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,11 +2,8 @@ def func(x):
     if(x % 15 == 0):
         return('fizzbuzz')
     elif(x % 5 == 0):
-        print('buzz')
         return 'buzz'
     elif(x % 3 == 0):
-        print('fizz')
         return 'fizz'
     else:
-        print(x)
         return x
